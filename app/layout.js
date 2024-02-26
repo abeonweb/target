b7b4e877a7ac2ba0b947697a5a949f29e85b1c2a
abeonweb <bmospecific@yahoo.com>
2024-02-26 16:27:03 +0100

    update to navigation import

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "../components/Navbar";
+import Navigation from "../components/navigation";
 import Footer from "../components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
+        <Navigation />
         {children}
         <Footer />
       </body>
