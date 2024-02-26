d2bed193aeafca6141f2a36f81ddd5f135ecb488
abeonweb <bmospecific@yahoo.com>
2024-02-26 16:28:24 +0100

    styled the component

--- app/user/page.jsx
+++ app/user/page.jsx
@@ -1,14 +1,18 @@
-import { createClient } from "../../utils/supabase/server";
-import { redirect } from "next/navigation";
+// import { createClient } from "../../utils/supabase/server";
+// import { redirect } from "next/navigation";
 
 const UserDashboard = async () => {
-  const supabase = createClient();
+  // const supabase = createClient();
 
-  const { data, error } = await supabase.auth.getUser();
-  if (error || !data?.user) {
-    redirect("/");
-  }
-  return <div className="pt-24">UserDashboard</div>;
+  // const { data, error } = await supabase.auth.getUser();
+  // if (error || !data?.user) {
+  //   redirect("/");
+  // }
+  return (
+    <div className="pt-24 h-[90vh]">
+      <h1>Welcome to your Dashboard</h1>
+    </div>
+  );
 };
 
 export default UserDashboard;
